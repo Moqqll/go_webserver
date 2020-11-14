11effbd407dc4c1c7ff90671cd27c80550a05d00
Moqqll <330934310@qq.com>
2020-11-14 19:23:20 +0800

    20201114 03

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go_webserver/dbops/mysql"
 	"go_webserver/dbops/redis"
@@ -8,8 +9,13 @@ import (
 	"go_webserver/route"
 	"go_webserver/setting"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -41,16 +47,31 @@ func main() {
 	//注册路由
 	r := route.Setup()
 
-	//启动服务（优雅关机）
+	//设置启动服务
 	srv := &http.Server{
-		Addr:    fmt.Sprintf("%d", viper.GetInt("app.port")),
+		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
 		Handler: r,
 	}
 
-	go func(){
-		//开启一个goroutine启动服务
-		if err:=srv.ListenAndServe();err!=nil&&err!=http.ErrServerClosed{
-			
+	go func() {
+		//开启一个goroutine来启动服务
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			zap.L().Error("listen:%s\n", zap.Error(err))
 		}
+	}()
+
+	//等待中断信号来优雅地关闭服务器，为关闭服务器设置一个5秒的超时
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM) //此处不会阻塞
+	<-quit                                               //阻塞在此，当接收到上述两种信号时才会往下执行
+
+	zap.L().Info("Shutdown server ...")
+	//创建一个5秒超时的context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	//优雅关机：5秒内优雅关闭服务器（待未处理完的请求处理完再关闭服务器），超过5秒则直接退出
+	if err := srv.Shutdown(ctx); err != nil {
+		zap.L().Error("Server Shutdown:%s\n", zap.Error(err))
 	}
+	zap.L().Info("Server exiting.")
 }
